cf51b8abeebf184aede47059e1eb47b72c9c277a
ahmad <ahmad.shekha26@gmail.com>
2024-08-27 18:55:42 +0300

    adding xverse

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import ConnectXverseWalletButton from '../src/Components/ConnectXverseWalletButton.jsx'
-import ConnectWalletButtons from '../src/Components/ConnectLedgerAndHiro.jsx'
 
 
 function App() {
